be99a7fbac3c478da16dcf753882a38c2ba743ff
najimi <gardin.quentin.pro@gmail.com>
2022-09-11 22:13:25 +0200

    add rows with parameters checks

--- src/utils/i18n/load_locale.rs
+++ src/utils/i18n/load_locale.rs
@@ -20,12 +20,13 @@ pub fn load_locales() -> HashMap<String, Locale>
     if files.is_empty()
     {
         println!(
-            "{} No locale files found. The bot needs at least one locale file to load.",
+            "{} No locale files found. The bot needs at least one locale file to load. en-US recommended.",
             "ERROR ".bright_red()
         );
         process::exit(1);
     }
 
+    // For all files in locales
     for file in files
     {
         match fetch_locale_from_file(&file)
@@ -34,8 +35,36 @@ pub fn load_locales() -> HashMap<String, Locale>
             {
                 let file_name = file.file_stem().unwrap().to_str().unwrap();
 
+                // If lang code is supported
                 if let Some(lang_code) = supported_language::try_get_lang_code(file_name)
                 {
+                    // Checking rows that should have parameters
+                    let roll_text_1 = &locale
+                        .translations
+                        .commands
+                        .roll
+                        .roll_details
+                        .rolling_multiple_dice;
+                    let roll_text_2 = &locale
+                        .translations
+                        .commands
+                        .roll
+                        .roll_details
+                        .rolling_single_die;
+
+                    if !roll_text_1.contains("{amount}")
+                        | !roll_text_1.contains("{size}")
+                        | !roll_text_2.contains("{size}")
+                    {
+                        println!(
+                            "{} Missing parameter in one of the translations of \"{}\".",
+                            "ERROR ".bright_red(),
+                            &file_name
+                        );
+                        process::exit(1);
+                    }
+
+                    // Insert locale in locales if everything's good.
                     println!("Loaded {}", format!("{}.json", file_name).yellow());
                     locales.insert(lang_code.to_str().to_string(), locale);
                 }
@@ -50,6 +79,7 @@ pub fn load_locales() -> HashMap<String, Locale>
                 }
             }
 
+            // Couldn't load locale from file
             Err(err) =>
             {
                 println!(
