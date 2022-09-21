4d010347988b38e23ff6db73a504f5681df80ddc
najimi <gardin.quentin.pro@gmail.com>
2022-09-21 19:37:13 +0200

    add async

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,7 +1,9 @@
 use serenity::prelude::Context;
 pub mod roll;
+pub mod setlang;
 
-pub fn register_commands(ctx: &Context)
+pub async fn register_commands(ctx: &Context)
 {
-    let _ = roll::register(ctx);
+    let _ = roll::register(ctx).await;
+    let _ = setlang::register(ctx).await;
 }
